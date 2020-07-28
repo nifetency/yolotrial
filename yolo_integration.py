d936f3ad69f89ce66d3a03555807a1502e70c66d
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2020-07-28 21:33:08 +0530

    Add files via upload

---
+++ yolo_integration.py
@@ -0,0 +1,112 @@
+import io
+import socket
+import struct
+from PIL import Image
+import matplotlib.pyplot as pl
+import cv2
+import numpy as np
+import time
+import logging
+
+
+
+server_socket = socket.socket()
+server_socket.bind(('106.220.162.180', 8000))  # ADD IP HERE
+server_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+
+logging.basicConfig(filename="Loglatency.log", level=logging.INFO)
+
+net = cv2.dnn.readNet('yolov3.weights','yolov3.cfg')
+classes = []
+with open('coco.names','r') as f:
+    classes = f.read().splitlines()
+
+
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+colors = np.random.uniform(0, 255, size=(100, 3))
+try:
+    img = None
+    while True:
+        # Read the length of the image as a 32-bit unsigned int. If the
+        # length is zero, quit the loop
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        if not image_len:
+            break
+        # Construct a stream to hold the image data and read the image
+        # data from the connection
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(image_len))
+        # Rewind the stream, open it as an image with PIL and do some
+        # processing on it
+        image_stream.seek(0)
+        image = Image.open(image_stream)
+
+        if img is None:
+            img = pl.imshow(image)
+        else:
+            img.set_data(image)
+
+        t = time.time()
+        _, img = cap.read()
+        height, width, _ = img.shape
+
+        blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+        net.setInput(blob)
+        output_layers_names = net.getUnconnectedOutLayersNames()
+        layerOutputs = net.forward(output_layers_names)
+
+        boxes = []
+        confidences = []
+        class_ids = []
+
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.2:
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append((float(confidence)))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
+
+        if len(indexes) > 0:
+            for i in indexes.flatten():
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                confidence = str(round(confidences[i], 2))
+                color = colors[i]
+                cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(img, label + " " + confidence, (x, y + 20), font, 2, (255, 255, 255), 2)
+
+        t1 = time.time()
+        tf = t1 - t
+        logging.info('{}'.format(tf))
+        cv2.imshow('Image', img)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+
+
+finally:
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    connection.close()
+    server_socket.close()
